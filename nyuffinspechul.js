19c91db87dae70428100a5c6b64df8dc0c14c027
MerileStudiosOfficial <sonichedgehog1@hotmail.com>
2016-07-10 06:16:41 -0500

    Update nyuffinspechul.js

--- nyuffinspechul.js
+++ nyuffinspechul.js
@@ -1,12 +1,3 @@
-// ==UserScript==
-// @name         New Userscript
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
-// @author       You
-// @match        https://cytu.be/*
-// @grant        none
-// ==/UserScript==
 
 with (typeof __commandLineAPI !== 'undefined' ? __commandLineAPI : { __proto__: null }) {
 /*
